7c3e2273af34620ee2f0f05e7710e52957ee00cf
Test1 <datphan82thuduc@gmail.com>
2020-01-17 10:08:43 +0700

    Update Face Regconize component and service
    Fixed issues

--- src/app/views/faceregconize/faceregconize.component.ts
+++ src/app/views/faceregconize/faceregconize.component.ts
@@ -8,6 +8,7 @@ import { FaceRegconizeService } from '../../services/faceregconize.service';
 export class FaceRegconizeComponent implements OnInit {
   @ViewChild('file', {static: false}) file: ElementRef;
   @ViewChild('img', {static: false}) img: ElementRef;
+  @ViewChild('imgPreview', {static: false}) imgPreview: ElementRef;
   constructor(private faceService: FaceRegconizeService) {
   }
 
@@ -19,6 +20,31 @@ export class FaceRegconizeComponent implements OnInit {
 	let fileToUpload = this.file.nativeElement.files[0];
 	const formData = new FormData();
 	formData.append('file', fileToUpload, fileToUpload.name);
-	this.faceService.ProcessImage(formData).subscribe((data) => { _this.img.nativeElement.src = 'data:image/jpeg;base64,' + data['_buffer'];});
+	this.faceService.ProcessImage(formData).subscribe((data) => { 
+    _this.img.nativeElement.innerHTML = '';
+		_this.img.nativeElement.insertAdjacentHTML('beforeend', '<img style="height:100%;" src="' + 'data:image/jpeg;base64,' + data['_buffer'] + '" />');
+	   });
+  }
+
+  openFileDialog() {
+	this.file.nativeElement.click();
+  }
+
+  previewImage() {
+    if (this.file.nativeElement.files.length === 0)
+      return;
+ 
+    const mimeType = this.file.nativeElement.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+    const _this = this;	
+    _this.imgPreview.nativeElement.innerHTML = '';
+    const reader = new FileReader();
+    reader.readAsDataURL(this.file.nativeElement.files[0]); 
+    reader.onload = (_event) => { 
+      _this.imgPreview.nativeElement.insertAdjacentHTML('beforeend', '<img style="height:100%;" src="' + reader.result + '" />'); 
+      _this.file.nativeElement.text = '';
+    };
   }
 }
